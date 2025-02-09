67eca4df76fa74065ce125f053918260781d11f6
hakmoune <mehdi.hakmoune@gmail.com>
2025-02-09 18:46:53 +0100

    Providers/Services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,17 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller('users') // route /users
+@Controller('users') // base route /users
 export class UsersController {
+  // The UsersService is injected into the controller via the constructor.
+  // NestJS takes care of creating an instance of UsersService and passing it to the controller.
+  constructor(private readonly userService: UsersService) {} // Dependency Injection
+
   @Get() // GET /users || GET /users?role=value
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.userService.findAll(role);
   }
 
   @Get('interns') // GET /users/interns
@@ -23,21 +28,36 @@ export class UsersController {
 
   @Get(':id') // GET /users/:id (Dynamic Routes)
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.userService.findOne(+id); // +id shorthand for converting a string to a number
   }
 
   @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.userService.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() user: {}) {
-    return { id, ...user };
+  update(
+    @Param('id') id: string,
+    @Body()
+    user: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.userService.update(+id, user);
   }
 
   @Delete(':id') // DELETE /users/:id
-  deleteOne(@Param('id') id: string) {
-    return { id };
+  delete(@Param('id') id: string) {
+    return this.userService.delete(+id);
   }
 }
